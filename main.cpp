57342f79a1a6e0f1247b4cf0c8bb0acc8745057d
Phillip Deremer <phillip@deremer.de>
2024-09-08 04:31:28 +0200

    Basically done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <locale.h>
 #include <ncursesw/ncurses.h>
 #include <string>
+#include <sys/stat.h>
+
+#include "monkey.hpp"
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -13,14 +16,11 @@
 
 using namespace std;
 
-/* A long int buffer containing the ascii animation of a dancing monkey for the mvaddstr function to get */
-const char* monkey = "  , - ~ ~ ~ - ,\n" \
-                     "  , '      ' , \n" \
-                     " ,           , \n" \
-                     " ,           , \n" \
-                     " ,           , \n";
+const string FILE_NAME = "kill.me";
 
 void quit();
+void calcCenter(int, int, int&, int&);
+bool exists(string name);
 
 /**
  * Project: Dancing-Monkey
@@ -52,18 +52,69 @@ int main(int argc, char* argv[]) {
 
   nodelay(win, true);
 
-  mvaddstr( 3, 5, monkey);
+  int width, height;
+  getmaxyx(stdscr, height, width);
+
+  int frame = 0;
+  int frameMilliSeconds = 1000000 / MONKEY_FPS;
+  int x, y;
+  calcCenter(width, height, x, y);
 
   while (true) {
-    int input = getch();
+    // int input = getch(); depricated! add timeout!
+    int input = wgetch(win);
+
+    for(int line = 0; line < MONKEY_HEIGHT - 1; line++) {
+      for(int col = 0; col < MONKEY_WIDTH; col++) {
+        const wchar_t c[3] = { MONKEY_FRAMES[((MONKEY_WIDTH * MONKEY_HEIGHT) * frame) + (line * MONKEY_WIDTH) + col], L' ', 0 };
+        mvaddwstr(line + y, col + x, c);
+      }
+    }
+    frame++;
+    if (frame >= MONKEY_FRAME_COUNT) {
+      frame = 0;
+    }
+
+    // Print the Wellcome message
+    mvaddstr(y + MONKEY_HEIGHT + 1, x, "      Welcome");
+    mvaddstr(y + MONKEY_HEIGHT + 3, x, "Take a look at this");
+    mvaddstr(y + MONKEY_HEIGHT + 4, x, "  dancing monkey!");
+
     if (isDebug) {
       if (input == 'q' || input == 'Q' || input == 27) {
         break;
       }
+
+      // if size of the terminal has changed
+      if (input == KEY_RESIZE) {
+        clear();
+        getmaxyx(stdscr, height, width);
+        calcCenter(width, height, x, y);
+      }
     }
+
+    // Check if the file exists
+    if (exists(FILE_NAME)) {
+      if (remove(FILE_NAME.c_str()) == 0) {
+        break;
+      }
+    }
+
+    // Sleep for a while
+    usleep(frameMilliSeconds);
   }
 
   return 0;
+}
+
+bool exists(string name) {
+  struct stat buffer;
+  return (stat (name.c_str(), &buffer) == 0);
+}
+
+void calcCenter(int width, int height, int& x, int& y) {
+  x = (width - MONKEY_WIDTH) / 2;
+  y = (height - MONKEY_HEIGHT - 4) / 2;
 }
 
 void quit() {
